3b8c3c8a2c6baa38c5755e5e97dac10ad6417267
cheezzjazz <vban@naver.com>
2020-01-10 18:47:25 +0900

    own dataset 만들기, 모듈화 과정 중

---
+++ Read_data_set.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import numpy as np
+
+dataset_train_path = "./dataset/position_left/"
+dataset_test_path = "./dataset/position_right/"
+
+train_x_data = np.zeros(0, dtype = np.float32)
+train_y_data = np.zeros(0, dtype = np.float32)
+test_x_data = np.zeros(0, dtype = np.float32)
+test_y_data = np.zeros(0, dtype = np.float32)
+
+# readfile
+def makeData(dataset_path, x_data, y_data):
+    i = 0
+    nframe = 1000
+    while i < nframe:
+        i += 1
+        file_name = 'Frame' + str(i) + '.p'
+        #print(file_name+'----------------------')
+        f = open(dataset_path+file_name, 'r') 
+        frame_array = np.zeros(0, dtype = np.float32)
+        while True: ################################### FrameN.p
+            line = f.readline()
+            if not line: break
+            else:################### line(node N xyz)
+                pos = np.fromstring(line, dtype = np.float32, sep= ' ')
+                frame_array = np.append(frame_array, pos)
+        f.close()
+        #print('frame %d'%i)
+        #print(frame_array)
+
+        if i < nframe:
+            #print('train x , frame %d'%i)
+            x_data = np.append(x_data, frame_array)
+            if i > 1:
+                #print('train y, frame %d' % i)
+                y_data = np.append(y_data, frame_array)
+        else:
+            #print('train y, frame %d' % i)
+            y_data = np.append(y_data, frame_array)
+
+        x_data = np.reshape(x_data, (-1, 60))
+        y_data = np.reshape(y_data, (-1, 60))
+        #print('x')
+        #print(x_data)
+        #print('y')
+        #print(y_data)
+
+makeData(dataset_train_path, train_x_data, train_y_data)
+print('Train')
+print(train_x_data)
+print(train_y_data)
+makeData(dataset_test_path, test_x_data, test_y_data)
+print('Test')
+print(test_x_data)
+print(test_y_data)
+
