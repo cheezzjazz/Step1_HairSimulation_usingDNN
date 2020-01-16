3ac799adfdae175a7879ede3d2f73eaeee0ffc9b
cheezzjazz <vban@naver.com>
2020-01-17 01:26:10 +0900

    학습모델 1차 완성

--- Model3_dropout.py
+++ Model3_dropout.py
@@ -31,8 +31,8 @@ def next_batch(num, data, labels):
 # 신경망 모델 구성 #
 ####################
 
-X = tf.placeholder(tf.float32,[999, 57])# [None, 60])
-Y = tf.placeholder(tf.float32,[999, 57])# [None, 60])
+X = tf.placeholder(tf.float32,[None, 57])# [None, 60])
+Y = tf.placeholder(tf.float32,[None, 57])# [None, 60])
 keep_prob = tf.placeholder(tf.float32)
 
 W1 = tf.Variable(tf.random_normal([57, 256], stddev = 0.01))#60, 256], stddev=0.01))
@@ -45,7 +45,7 @@ L2 = tf.nn.dropout(L2, keep_prob)
 
 W3 = tf.Variable(tf.random_normal([256,57], stddev = 0.01))# 60], stddev=0.01))
 model = tf.matmul(L2, W3)
-print(model)
+
 # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 cost = tf.reduce_mean(tf.square(model - Y))
 optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
@@ -63,7 +63,7 @@ total_batch = int(999 / batch_size)
 #total_batch = int(mnist.train.num_examples / batch_size)
 
 #for epoch in range(15): # dropout 사용 전
-for epoch in range(30):
+for epoch in range(100):
     total_cost = 0
 
     for i in range(total_batch):
@@ -87,6 +87,7 @@ print('최적화 완료!')
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 # print('정확도:', sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels})) # dropout 사용 전
-print('정확도:', sess.run(accuracy, feed_dict={X: test_x_data, Y: test_y_data, keep_prob: 1}))
+print('Training 정확도:', sess.run(accuracy, feed_dict={X: train_x_data, Y: train_y_data, keep_prob:1}))
+print('Test 정확도:', sess.run(accuracy, feed_dict={X: test_x_data, Y: test_y_data, keep_prob: 1}))
 
 
