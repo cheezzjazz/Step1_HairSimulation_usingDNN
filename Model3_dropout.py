5dcd66db58b51721857bd5a70aa40d39efa81bbc
cheezzjazz <vban@naver.com>
2020-01-17 02:20:52 +0900

    학습 모델 cost 0.002

--- Model3_dropout.py
+++ Model3_dropout.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 import numpy as np
 
 from Read_data_set import *
@@ -16,38 +15,33 @@ def next_batch(num, data, labels):
     labels_shuffle = [labels[ i] for i in idx]
     
     return np.asarray(data_shuffle), np.asarray(labels_shuffle)
-#print('x-----------------------\n')
-#print(train_x_data)
-#print('y----------------------\n')
-#print(train_y_data)
-#batch_x, batch_y = next_batch(100, train_x_data, train_y_data)
-#print('batch x-----------------\n')
-#print(batch_x.shape)
-#print('batch y-----------------\n')
-#print(batch_y.shape)
+
 ####################
 # 신경망 모델 구성 #
 ####################
+#global_step = tf.Variable(0, trainable=False, name='global_step')
 
 X = tf.placeholder(tf.float32,[None, 57])# [None, 60])
 Y = tf.placeholder(tf.float32,[None, 57])# [None, 60])
 keep_prob = tf.placeholder(tf.float32)
 
-W1 = tf.Variable(tf.random_normal([57, 256], stddev = 0.01))#60, 256], stddev=0.01))
-L1 = tf.nn.relu(tf.matmul(X, W1))
-L1 = tf.nn.dropout(L1, keep_prob)
+with tf.name_scope('Layer1'):
+    W1 = tf.Variable(tf.random_normal([57, 256], stddev = 0.01))#60, 256], stddev=0.01))
+    L1 = tf.nn.relu(tf.matmul(X, W1))
+    L1 = tf.nn.dropout(L1, keep_prob)
+with tf.name_scope('Layer2'):
+    W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
+    L2 = tf.nn.relu(tf.matmul(L1, W2))
+    L2 = tf.nn.dropout(L2, keep_prob)
+with tf.name_scope('Output'):
+    W3 = tf.Variable(tf.random_normal([256,57], stddev = 0.01))# 60], stddev=0.01))
+    model = tf.matmul(L2, W3)
+with tf.name_scope('Optimizer'):
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
+    cost = tf.reduce_mean(tf.square(model - Y))
+    optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
-W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
-L2 = tf.nn.relu(tf.matmul(L1, W2))
-L2 = tf.nn.dropout(L2, keep_prob)
-
-W3 = tf.Variable(tf.random_normal([256,57], stddev = 0.01))# 60], stddev=0.01))
-model = tf.matmul(L2, W3)
-
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
-cost = tf.reduce_mean(tf.square(model - Y))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
-
+#    tf.summary.scalar('cost', cost)
 ####################
 # 신경망 모델 학습 #
 ####################
@@ -55,6 +49,9 @@ optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+
+#merged = tf.summary.merge_all()
+#writer = tf.summary.FileWriter('./logs', sess.graph)
 
 batch_size = 100
 total_batch = int(len(train_x_data) / batch_size)
@@ -70,9 +67,11 @@ for epoch in range(100):
         _, cost_val = sess.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys, keep_prob: 0.8})
         total_cost += cost_val 
 
+#        summary = sess.run(merged, feed_dict={X:batch_xs, Y:batch_ys})
+#        write.add_summary(summary, global_step = sess.run(global_step))
     print('Epoch:', '%04d' % (epoch + 1),
           'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
-
+    
 print('최적화 완료!')
 
 
@@ -84,7 +83,7 @@ print('최적화 완료!')
 is_correct = tf.equal(tf.argmax(model,1), tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 # print('정확도:', sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels})) # dropout 사용 전
-print('Training 정확도:', sess.run(accuracy, feed_dict={X: train_x_data, Y: train_y_data, keep_prob:1}))
+# print('Training 정확도:', sess.run(accuracy, feed_dict={X: train_x_data, Y: train_y_data, keep_prob:1}))
 print('Test 정확도:', sess.run(accuracy, feed_dict={X: test_x_data, Y: test_y_data, keep_prob: 1}))
 
 
